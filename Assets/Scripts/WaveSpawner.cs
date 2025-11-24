138337beecddfa2134f8e9a69c43ab8f3041cb87
Eddie Annor-Denkyira <EA867@student.aru.ac.uk>
2025-11-24 11:40:42 +0000

    New enemies and bug fixes

--- Assets/Scripts/WaveSpawner.cs
+++ Assets/Scripts/WaveSpawner.cs
@@ -2,10 +2,15 @@ using UnityEngine;
 using System.Collections;
 using NUnit.Framework.Constraints;
 using TMPro;
+using Unity.Mathematics;
+using System;
 
 public class WaveSpawner : MonoBehaviour
 {
     public Transform enemyPrefab;
+    public Transform slowPrefab;
+    public Transform fastPrefab;
+    public Transform deyuraPrefab;
     public Transform enemyFolder;
 
     public Transform spawnPoint;
@@ -44,19 +49,92 @@ public class WaveSpawner : MonoBehaviour
         countdown -= Time.deltaTime;
         countdown = Mathf.Clamp(countdown, 0f, Mathf.Infinity);
 
-        statusText.text = string.Format("WAVE " + waveIndex + " - {0:00.00}", countdown);
+        if (waveIndex < 30)
+        {
+            statusText.text = string.Format("WAVE " + waveIndex + " - {0:00.00}", countdown);
+        }
+        else
+        {
+            statusText.text = "THE FINAL WAVE.";
+            statusText.color = Color.red;
+        }
     }
 
     IEnumerator SpawnWave()
     {
         waveIndex++;
-        enemiesLeftToSpawn = waveIndex;
 
-        for (int i = 0; i < waveIndex; i++)
+        if (waveIndex <= 5)
         {
-            SpawnEnemy("Basic");
-            enemiesLeftToSpawn--;
-            yield return new WaitForSeconds(0.5f);
+            enemiesLeftToSpawn = waveIndex;
+
+            for (int i = 0; i < waveIndex; i++)
+            {
+                SpawnEnemy("Basic");
+
+                enemiesLeftToSpawn--;
+                yield return new WaitForSeconds(0.5f);
+            }
+        }
+        else if (waveIndex < 10)
+        {
+            enemiesLeftToSpawn = waveIndex + (waveIndex - 5);
+
+            for (int i = 0; i < waveIndex; i++)
+            {
+                SpawnEnemy("Basic");
+
+                enemiesLeftToSpawn--;
+                yield return new WaitForSeconds(0.5f);
+            }
+
+            for (int i = 0; i < (waveIndex - 5); i++)
+            {
+                SpawnEnemy("Fast");
+
+                enemiesLeftToSpawn--;
+                yield return new WaitForSeconds(0.5f);
+            }
+        }
+        else if (waveIndex < 20)
+        {
+            enemiesLeftToSpawn = waveIndex + (waveIndex - 5) + (waveIndex - 10);
+
+            for (int i = 0; i < waveIndex; i++)
+            {
+                SpawnEnemy("Basic");
+
+                enemiesLeftToSpawn--;
+                yield return new WaitForSeconds(0.5f);
+            }
+
+            for (int i = 0; i < (waveIndex - 5); i++)
+            {
+                SpawnEnemy("Fast");
+
+                enemiesLeftToSpawn--;
+                yield return new WaitForSeconds(0.5f);
+            }
+
+            for (int i = 0; i < (waveIndex - 10); i++)
+            {
+                SpawnEnemy("Slow");
+
+                enemiesLeftToSpawn--;
+                yield return new WaitForSeconds(0.5f);
+            }
+        }
+        else if (waveIndex < 30)
+        {
+            enemiesLeftToSpawn = 999999999;
+
+            while (true)
+            {
+                SpawnEnemy("Deyura");
+
+                enemiesLeftToSpawn--;
+                yield return new WaitForSeconds(0.5f);
+            }
         }
     }
 
@@ -65,6 +143,18 @@ public class WaveSpawner : MonoBehaviour
         if (EnemyType == "Basic")
         {
             Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation, enemyFolder);
+        }
+        else if (EnemyType == "Slow")
+        {
+            Instantiate(slowPrefab, spawnPoint.position, spawnPoint.rotation, enemyFolder);
+        }
+        else if (EnemyType == "Fast")
+        {
+            Instantiate(fastPrefab, spawnPoint.position, spawnPoint.rotation, enemyFolder);
+        }
+        else if (EnemyType == "Deyura")
+        {
+            Instantiate(deyuraPrefab, spawnPoint.position, spawnPoint.rotation, enemyFolder);
         }
     }
 }
